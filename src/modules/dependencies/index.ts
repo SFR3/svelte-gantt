6386267aa9c4ac31e159dc49c326ca3c2c7f036f
Michael Daugherty <mike@mpdaugherty.com>
2021-09-23 15:31:47 +0800

    Create an installable version of this package after building

--- src/modules/dependencies/index.ts
+++
@@ -1,7 +0,0 @@
-import _svelteGanttDependencies from './GanttDependencies.svelte';
-import { ComponentCreator, Component } from '../../core/svelte';
-import { DependencyModel } from './dependency';
-
-var SvelteGanttDependencies = _svelteGanttDependencies as unknown as ComponentCreator<Component>;
-
-export { SvelteGanttDependencies, DependencyModel };
--- src/modules/table/index.ts
+++
@@ -1,6 +0,0 @@
-import _svelteGanttTable from './Table.svelte';
-import { ComponentCreator, Component } from '../../core/svelte';
-
-var SvelteGanttTable = _svelteGanttTable as unknown as ComponentCreator<Component>;
-
-export { SvelteGanttTable };
